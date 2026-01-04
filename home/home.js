6b9dd9da8a6c042e9df1a7dd40f7165247db7331
sila <fatimasila83@gmail.cm>
2026-01-04 11:49:22 +0500

    homepageDone

---
+++ home/home.js
@@ -0,0 +1,27 @@
+let playbtn = document.querySelectorAll(".play-btn");
+let audios = document.querySelectorAll(".audio");
+
+playbtn.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+
+        audios.forEach((audio, i) => {
+            if (i !== index) {
+                audio.pause();
+                playbtn[i].textContent = "▶";
+            }
+        });
+
+        let currentAudio = audios[index];
+
+        if (currentAudio.paused) {
+            currentAudio.play();
+            btn.textContent = "⏸";
+        } else {
+            currentAudio.pause();
+            btn.textContent = "▶";
+        }
+        currentAudio.onended = () => {
+            btn.textContent = "▶";
+        }
+    });
+});
